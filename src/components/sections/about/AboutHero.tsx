72a7ace75e36b2b2bfd2bd5102464ea8c4358603
StarKolya <mikola.starovoit@gmail.com>
2026-09-24 23:11:45 +0200

    3

--- src/components/sections/about/AboutHero.tsx
+++ src/components/sections/about/AboutHero.tsx
@@ -16,11 +16,14 @@ export function AboutHero({ onCtaClick }: { onCtaClick: () => void }) {
 
       <div className="relative z-10 flex w-full flex-col justify-end px-5 pb-10 tablet:pb-16 desktop:px-16">
         <h1 className="w-fit font-semibold uppercase leading-none tracking-[-0.01em] text-[32px] text-white tablet:text-[56px] desktop:text-[80px]">
-          {t("titleMain1")}
-          <br />
-          {t("titleMain2")}
-          <br />
-          <span className="mt-1 inline-block rounded-md bg-white/70 px-2 py-1 text-brand-dark tablet:mt-4 tablet:px-4 tablet:py-2">
+          {t("titleMain1")}{" "}
+          <br className="tablet:hidden" />
+          {t("titleMain2")}{" "}
+          <br className="hidden tablet:inline" />
+          {t("titleMain3")}
+          <br className="tablet:hidden" />
+          {" "}
+          <span className="mt-1 inline-block rounded-md bg-white/70 px-2 py-1 text-brand-dark tablet:mt-2 tablet:px-4 tablet:py-2">
             {t("titleHighlight")}
           </span>
         </h1>
